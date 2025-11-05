eda13c3ada7c5336988919f1dfe39911994287a5
Neal Magee <nem2p@virginia.edu>
2025-11-05 15:13:23 -0500

    Tracking with redis

---
+++ wikipedia-consumer.py
@@ -0,0 +1,64 @@
+from quixstreams import Application
+import json
+import redis
+
+def main():
+    app = Application(
+        broker_address="127.0.0.1:19092,127.0.0.1:29092,127.0.0.1:39092",
+        loglevel="DEBUG",
+        consumer_group="wikipedia-consumer",
+        auto_offset_reset="earliest",
+        producer_extra_config={
+            # Resolve localhost to 127.0.0.1 to avoid IPv6 issues
+            "broker.address.family": "v4",
+        }
+    )
+
+    with app.get_consumer() as consumer:
+        consumer.subscribe(["wikipedia-changes"])
+
+        # new = 0
+        # edit = 0
+        # delete = 0
+        # move = 0
+        # protect = 0
+        # unprotect = 0
+        # revert = 0
+        # upload = 0
+        # log = 0
+        # categorize = 0
+        # other = 0
+
+        while True:
+            msg = consumer.poll(1)
+
+            if msg is None:
+                print("Waiting...")
+            elif msg.error() is not None:
+                raise Exception(msg.error())
+            else:
+                key = msg.key().decode("utf8")
+                value = json.loads(msg.value())
+                offset = msg.offset()
+
+                change_type = value.get("type")
+                # save these values to redis and use incr() to increment the value
+                redis_client = redis.Redis(host="localhost", port=6379, db=0)
+                redis_client.incr(change_type)
+                print(f"Type count: {redis_client.get(change_type).decode('utf8')}")
+                redis_client.close()
+
+                print(f"{offset} {key} {change_type}")
+                # print(f"{offset} {key} {value}")
+                consumer.store_offsets(msg)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        redis_client = redis.Redis(host="localhost", port=6379, db=0)
+        # for each key in redis, print the key and value
+        for key in redis_client.keys():
+            print(f"Type: {key.decode('utf8')} | Count: {redis_client.get(key).decode('utf8')}")
+        redis_client.close()
+        pass
